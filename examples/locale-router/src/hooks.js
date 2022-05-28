bab9b1014a8c45e055297d58692c47546c7dd368
jarda-svoboda <20700514+jarda-svoboda@users.noreply.github.com>
2022-05-28 14:21:22 +0000

    Add fetch

--- examples/locale-router/src/hooks.js
+++ examples/locale-router/src/hooks.js
@@ -6,7 +6,7 @@ const routeRegex = new RegExp(/^\/[^.]*([?#].*)?$/);
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
   const { url, request } = event;
-  const { pathname } = url;
+  const { pathname, origin } = url;
 
   // If this request is a route request
   if (routeRegex.test(pathname)) {
@@ -18,18 +18,21 @@ export const handle = async ({ event, resolve }) => {
 
     // If route locale is not supported
     if (!locale) {
-      // Get user preferred locale
-      locale = `${`${request.headers.get('accept-language')}`.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
-
       // Set default locale if user preferred locale does not match
-      if (!supportedLocales.includes(locale)) locale = defaultLocale;
+      locale = defaultLocale;
 
-      // 301 redirect
-      return new Response(undefined, { headers: { 'location': `/${locale}${pathname}` }, status: 301 });
+      // Replace the trailing slash as `svelte.config` is set to `trailingSlash: 'never'`
+      const redirectTo = `${origin}/${locale}${pathname}`.replace(/\/$/, '');
+
+      // Serve the redirected route.
+      // In this case we don't have to set the html 'lang' attribute
+      // as the default locale is already included in our app.html.
+      return fetch(redirectTo, request);
     }
 
+
     // Add html `lang` attribute
-    return resolve(eventToResolve, {
+    return resolve(event, {
       transformPage: ({ html }) => html.replace(/<html.*>/, `<html lang="${locale}">`),
     });
   }
